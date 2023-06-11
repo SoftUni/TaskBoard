48fbb1747c70076997320db77b137159a751510e
Denis Vasilev <vassdeniss@gmail.com>
2023-06-12 00:54:12 +0300

    Turned properties into fields

--- TaskBoard.Data/ApplicationDbContext.cs
+++ TaskBoard.Data/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
@@ -9,10 +9,11 @@ namespace TaskBoard.Data
     public class ApplicationDbContext : IdentityDbContext<User>
     {
         private bool seedDb = true;
-        private User GuestUser { get; set; }
-        private Board OpenBoard { get; set; }
-        private Board InProgressBoard { get; set; }
-        private Board DoneBoard { get; set; }
+
+        private User guestUser;
+        private Board openBoard;
+        private Board inProgressBoard;
+        private Board doneBoard;
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, bool seedDb = true)
             : base(options)
@@ -22,6 +23,7 @@ namespace TaskBoard.Data
         }
 
         public DbSet<Board> Boards { get; set; }
+
         public DbSet<Task> Tasks { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
@@ -29,20 +31,20 @@ namespace TaskBoard.Data
             if (seedDb)
             {
                 builder
-                .Entity<Task>()
-                .HasOne(t => t.Board)
-                .WithMany(b => b.Tasks)
-                .HasForeignKey(t => t.BoardId)
-                .OnDelete(DeleteBehavior.Restrict);
+                    .Entity<Task>()
+                    .HasOne(t => t.Board)
+                    .WithMany(b => b.Tasks)
+                    .HasForeignKey(t => t.BoardId)
+                    .OnDelete(DeleteBehavior.Restrict);
 
                 SeedBoards();
                 builder
                     .Entity<Board>()
-                    .HasData(this.OpenBoard, this.InProgressBoard, this.DoneBoard);
+                    .HasData(this.openBoard, this.inProgressBoard, this.doneBoard);
 
                 SeedUsers();
                 builder.Entity<User>()
-                    .HasData(this.GuestUser);
+                    .HasData(this.guestUser);
 
                 builder
                     .Entity<Task>()
@@ -52,8 +54,8 @@ namespace TaskBoard.Data
                         Title = "Improve CSS styles",
                         Description = "Implement better styling for all public pages",
                         CreatedOn = DateTime.Now.AddDays(-200),
-                        OwnerId = this.GuestUser.Id,
-                        BoardId = this.OpenBoard.Id
+                        OwnerId = this.guestUser.Id,
+                        BoardId = this.openBoard.Id
                     },
                     new Task()
                     {
@@ -61,8 +63,8 @@ namespace TaskBoard.Data
                         Title = "Android Client App",
                         Description = "Create Android client app for the TaskBoard RESTful API",
                         CreatedOn = DateTime.Now.AddMonths(-5),
-                        OwnerId = this.GuestUser.Id,
-                        BoardId = this.OpenBoard.Id
+                        OwnerId = this.guestUser.Id,
+                        BoardId = this.openBoard.Id
                     },
                     new Task()
                     {
@@ -70,8 +72,8 @@ namespace TaskBoard.Data
                         Title = "Desktop Client App",
                         Description = "Create Windows Forms desktop app client for the TaskBoard RESTful API",
                         CreatedOn = DateTime.Now.AddMonths(-1),
-                        OwnerId = this.GuestUser.Id,
-                        BoardId = this.InProgressBoard.Id
+                        OwnerId = this.guestUser.Id,
+                        BoardId = this.inProgressBoard.Id
                     },
                     new Task()
                     {
@@ -79,8 +81,8 @@ namespace TaskBoard.Data
                         Title = "Create Task",
                         Description = "Implement [Create Task] page for adding new tasks",
                         CreatedOn = DateTime.Now.AddYears(-1),
-                        OwnerId = this.GuestUser.Id,
-                        BoardId = this.DoneBoard.Id
+                        OwnerId = this.guestUser.Id,
+                        BoardId = this.doneBoard.Id
                     });
             }
 
@@ -91,7 +93,7 @@ namespace TaskBoard.Data
         {
             var hasher = new PasswordHasher<User>();
 
-            this.GuestUser = new User()
+            this.guestUser = new User()
             {
                 UserName = "guest",
                 NormalizedUserName = "GUEST",
@@ -101,24 +103,24 @@ namespace TaskBoard.Data
                 LastName = "User",
             };
 
-            this.GuestUser.PasswordHash = hasher.HashPassword(this.GuestUser, "guest");
+            this.guestUser.PasswordHash = hasher.HashPassword(this.guestUser, "guest");
         }
 
         private void SeedBoards()
         {
-            this.OpenBoard = new Board()
+            this.openBoard = new Board()
             {
                 Id = 1,
                 Name = "Open"
             };
 
-            this.InProgressBoard = new Board()
+            this.inProgressBoard = new Board()
             {
                 Id = 2,
                 Name = "In Progress"
             };
 
-            this.DoneBoard = new Board()
+            this.doneBoard = new Board()
             {
                 Id = 3,
                 Name = "Done"
